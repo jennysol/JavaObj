040c7d703b3e8e76a820c2725d132a9c6a34315f
jennysol <jennyssoliver@gmail.com>
2021-04-21 18:44:14 -0300

    Iniciando Janela Black

--- Jogo/src/game/Game.java
+++ Jogo/src/game/Game.java
@@ -1,27 +1,42 @@
 package game;
 
 import java.awt.Canvas;
-
+import java.awt.Color;
 import java.awt.Dimension;
-
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	
+	public static final int WIDTH = 640, HEIGHT = 480;
+	
+	
 	//Método Construtor
 	public Game() {
-		Dimension dimension = new Dimension(640, 480);
+		Dimension dimension = new Dimension(WIDTH,HEIGHT);
 		this.setPreferredSize(dimension);
 	}
 	
 	// Método de Atualização
 	public void update() {
-		
+		System.out.println("Rodando game looping.");
 	}
 	
 	//Método para Renderização
 	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
 		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
@@ -36,6 +51,8 @@ public class Game extends Canvas implements Runnable {
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		jframe.setVisible(true);
+		
+		new Thread(game).start();
 	}
 	
 	@Override
