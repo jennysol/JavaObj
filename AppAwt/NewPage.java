a174d6ca21cef838e14190c3a0080b43e343f632
jennysol <jennyssoliver@gmail.com>
2021-10-04 07:29:27 -0300

    Feature
    Add awt new page

---
+++ AppAwt/NewPage.java
@@ -0,0 +1,15 @@
+package app;
+
+import javax.swing.*;  
+import java.awt.*;  
+
+public class NewPage extends JFrame  {
+	//constructor  
+    NewPage()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Welcome");  
+        setSize(400, 200);  
+    } 
+}
