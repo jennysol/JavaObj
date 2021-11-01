327d86b3fb2f3c188423f413ff955a9906bed76c
jennysol <jennyssoliver@gmail.com>
2021-11-01 14:53:36 -0300

    Feature
    Add Test for modelo

---
+++ Course01/Modelo/Test.java
@@ -0,0 +1,11 @@
+// Importando all members public static de Utils
+import static model.Utils.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int x = VALUE;
+        methos1();
+        method2(x);
+    }
+}
