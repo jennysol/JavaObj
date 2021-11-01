b9f57ee68e67200f7afffd2149f486bbd80a72ff
jennysol <jennyssoliver@gmail.com>
2021-11-01 14:52:01 -0300

    Feature
    Add Utils in Modelo

---
+++ Course01/Modelo/Utils.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Utils {
+
+    // Atributo estático público 
+    public static int VALOR = 0;
+    // Métodos estáticos públicos
+    public static void metodo1() {}
+    public static void metodo1(int a) {}
+
+}
